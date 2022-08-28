907a0f5e42b97180828c497a99a64dab8e142baa
putusel <elina.putus@gmail.com>
2022-08-28 19:15:38 +0300

    Tehtävä lähes valmis

--- App.js
+++ App.js
@@ -1,11 +1,45 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState } from 'react';
+import { StyleSheet, View, Button, TextInput, Image, Text, mathjs } from 'react-native';
 
 export default function App() {
+  const [text, setText] = useState('');
+  const [text2, setText2] = useState('');
+  const [result, setData] = useState('');
+    
+
+  const buttonPressed = () => { 
+    const result = Number(text)+Number(text2);
+    setData(result);
+  };
+  const buttonPressed2 = () => { 
+    const result = Number(text)-Number(text2);
+    setData(result);
+  };
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <Image style={styles.image} source={{uri: 'https://th.bing.com/th/id/OIP.vumzh-lDnQ6Bgr6rp1NzkAHaHa?w=209&h=209&c=7&r=0&o=5&dpr=1.5&pid=1.7'}} />
+      <Text >Result: {result} </Text>
+        <TextInput 
+          style={styles.input} 
+          keyboardType={'numeric'}
+          placeholder="enter a number"
+          onChangeText={text => setText(text)} value={text}
+        />
+        <TextInput 
+          style={styles.input}
+          keyboardType={'numeric'}
+          placeholder="enter a number"
+          onChangeText={text2 => setText2(text2)} value={text2}
+        />
+        <View style={{ flexDirection: 'row', justifyContent: 'space-around', marginTop: 10, marginBottom: 10 }}>
+          <Button onPress={buttonPressed} title="+" />
+          <Button onPress={buttonPressed2} title="-" />
+        </View>
+        
+        
+       <StatusBar style="auto" />
     </View>
   );
 }
@@ -17,4 +51,18 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
+  image : {
+    width: 250,
+    height: 100,
+    marginTop: 10,
+    marginBottom: 10
+    
+  },
+  input : {
+    width:200, 
+    borderColor: 'gray', 
+    borderWidth: 1,
+    
+  },
+   
 });
