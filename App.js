5b0e49842f028345989e3f1449657aa3815f6c26
putusel <elina.putus@gmail.com>
2022-09-03 20:37:47 +0300

    Tehtävä valmis

--- App.js
+++ App.js
@@ -1,20 +1,27 @@
 import { StatusBar } from 'expo-status-bar';
 import React, { useState } from 'react';
-import { StyleSheet, View, Button, TextInput, Image, Text, Dimensions } from 'react-native';
+import { StyleSheet, View, Button, TextInput, Image, Text, Dimensions, FlatList } from 'react-native';
 
 export default function App() {
   const [text, setText] = useState('');
   const [text2, setText2] = useState('');
   const [result, setData] = useState('');
-    
+  const [data, setData2] = useState([]);
 
   const buttonPressed = () => { 
     const result = Number(text)+Number(text2);
     setData(result);
+    const history = text.toString() + ' + ' + text2.toString() + ' = ' + result.toString();
+    setData2([...data, { key: history }]);
+    setText('');
   };
   const buttonPressed2 = () => { 
     const result = Number(text)-Number(text2);
     setData(result);
+    const history = text.toString() + ' - ' + text2.toString() + ' = ' + result.toString();
+    setData2([...data, { key: history }]);
+    setText('');
+    
   };
 
   return (
@@ -37,12 +44,16 @@ export default function App() {
           <Button onPress={buttonPressed} title="+" />
           <Button onPress={buttonPressed2} title="–" />
         </View>
-        
-        
+        <Text style={styles.text}>History</Text>
+        <FlatList style={styles.list}
+          data={data}
+          renderItem={({ item }) =>
+            <Text>{item.key}</Text>
+        }/>
        <StatusBar style="auto" />
     </View>
-  );
-}
+    );
+  }      
 
 const styles = StyleSheet.create({
   container: {
